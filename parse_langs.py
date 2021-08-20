46e9c9c06eb64003385d010b1e7d554827c37d5d
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2021-08-20 07:09:03 +0300

    Loop through keys

--- parse_langs.py
+++ parse_langs.py
@@ -3,4 +3,12 @@ import json
 with open("languages.json", encoding="utf8") as f:
     data = json.load(f)
 
-print(data["translation"])
+
+translation = data["translation"]
+
+for key in translation:
+    value = data[key]
+    # print("The key and value are ({}) = ({})".format(key, value))
+    print(key)
+
+# print(data["translation"]["af"])
