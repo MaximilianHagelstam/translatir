1e3bbef87f66a7ced1963d0a193d64231013c970
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2021-08-20 07:21:46 +0300

    Checkpoint

--- parse_langs.py
+++ parse_langs.py
@@ -3,12 +3,6 @@ import json
 with open("languages.json", encoding="utf8") as f:
     data = json.load(f)
 
-
-translation = data["translation"]
-
-for key in translation:
+for key in data:
     value = data[key]
-    # print("The key and value are ({}) = ({})".format(key, value))
-    print(key)
-
-# print(data["translation"]["af"])
+    print('<option value="{}">{}</option>'.format(key, value))
